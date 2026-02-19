9752fbcf34c405ebef64233413d9240932ef4872
makegoralex <makegoralex@yandex.ru>
2026-02-19 09:32:41 +0300

    Update build.gradle.kts

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yago.evotor"
         minSdk = 24
         targetSdk = 34
-        versionCode = 33
-        versionName = "0.3.1"
+        versionCode = 35
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
