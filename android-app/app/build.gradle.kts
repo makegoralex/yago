e2a7af2a75f1ffc81d234cc737544f0ac0d85e19
makegoralex <makegoralex@yandex.ru>
2026-02-19 09:30:29 +0300

    Populate Evotor sell receipt from pending order via before-positions event

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.github.evotor:integration-library:v0.4.8")
+    implementation("com.github.evotor:integration-library:v0.6.31")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
